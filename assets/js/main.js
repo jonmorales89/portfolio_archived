3d44cb74ae18caf2794ae1704586a45cf4ba5c34
jonmorales89 <jon.sal.morales@gmail.com>
2017-09-06 19:28:48 -0700

    zoom levels

--- assets/js/main.js
+++ assets/js/main.js
@@ -448,7 +448,7 @@ $(document).ready(function($){
 			lng : -117.830676,
 			scrollwheel: false,
 			draggable: draggableOp,
-			zoom: 10,
+			zoom: 13,
 			disableDefaultUI: true,
 			styles : mapStyle
 		});
